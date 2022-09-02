958521b23d3f99d1e0cb06bc6cf417990943d04f
Mbazu Daniel <mbazudaniel97@gmail.com>
2022-09-02 17:42:11 +0100

    Code: Counter Contract development

---
+++ hardhat.config.js
@@ -0,0 +1,15 @@
+require("@nomiclabs/hardhat-waffle"); // library from Hardhat to enable us write this kind of code
+require("dotenv").config({ path: ".env" }); // This gets the environment variables
+
+const ALCHEMY_API_KEY_URL = process.env.ALCHEMY_API_KEY_URL;
+const RINKEBY_PRIVATE_KEY = process.env.RINKEBY_PRIVATE_KEY;
+
+module.exports = {
+  solidity: "0.8.4",
+  networks: {
+    rinkeby: {
+      url: ALCHEMY_API_KEY_URL,
+      accounts: [RINKEBY_PRIVATE_KEY],
+    },
+  },
+};
